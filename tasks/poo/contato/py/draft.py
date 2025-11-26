b7b56919929e8218dadf5f8649b5709e3be41079
isabeliloliveira <isabeli.oliveira@alu.ufc.br>
2025-11-26 15:23:12 +0000

    summit

---
+++ tasks/poo/contato/py/draft.py
@@ -0,0 +1,20 @@
+class Fone:
+    def __init__(self, id: str, number: str):
+        self.__id=id
+        self.__number=number
+
+    def isValid(self) -> bool:
+
+    def get_id(self):
+        return self.__id
+    
+    def get_number(self):
+        return self.__number
+    
+    def __str__(self):
+        return f""
+    
+class Contact:
+    def __init__(self, ):
+        
+        
